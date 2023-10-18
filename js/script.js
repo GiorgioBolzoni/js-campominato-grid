1ed652798eba8d6cfb1cf4ce5157e7c4c4e8219d
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-18 15:14:40 +0200

    importata prima parte esercizio con scaffolding incluso

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*
+al click su btn
+generare 64 quadratini
+
+al click sul quadratino lo sfondo del quadratino stesso deve diventare verde
+
+*/
+
+
+/* 
+<div class="square"></div> 
+*/
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    //numero di quadratini da generare
+    const numSquare = 64;
+    // mi prendo la griglia di gioco
+    const playground = document.getElementById('playground')
+    //ciclo per stampare i quadratini
+    for (let i = 0; i < numSquare; i++) {
+        //genero quadratino
+        let square = drawSquare(i, numSquare);
+        console.log(square);
+
+        //appendo il quadratino alla griglia (playground)
+        playground.append(square);
+    };
+});
+
+function drawSquare(squareIndex, numSquare) {
+    squareWidth = Math.sqrt(numSquare);
+    console.log(squareWidth);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${squareWidth})`;
+    square.style.height = `calc(100% / ${squareWidth})`;
+    square.innerHTML = squareIndex + 1;
+    square.addEventListener('click', function () {
+        square.classList.add('active');
+        square.style.color = 'black';
+    });
+    return square;
+};
